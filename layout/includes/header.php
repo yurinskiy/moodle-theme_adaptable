6fad6d0ceb6af7c219a7f122a48a2c4c0d8aacef
Manoj Solanki <ac5882@coventry.ac.uk>
2019-03-06 14:46:12 +0000

    Routine check-in.

--- layout/includes/header.php
+++ layout/includes/header.php
@@ -431,9 +431,9 @@ if (
                         </a>
                         <div class="nav-collapse collapse "> -->
 
-<div id="navwrap">
+<div id="main-navbar">
     <nav class="navbar navbar-expand-lg navbar-light bg-light btco-hover-menu">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     
