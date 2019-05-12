7f5a998d186f8e882c0f80325a999ce6a9844a49
3bits <info@3bits.es>
2019-05-12 22:44:19 +0200

    fix js issues

--- layout/includes/header.php
+++ layout/includes/header.php
@@ -58,7 +58,7 @@ if (isset($PAGE->theme->settings->stickynavbar) && $PAGE->theme->settings->stick
     $fixedheader = false;
 }
 
-$PAGE->requires->js_call_amd('theme_adaptable/bsoptions', 'init', array($fixedheader));
+//$PAGE->requires->js_call_amd('theme_adaptable/bsoptions', 'init', array($fixedheader));
 $PAGE->requires->js_call_amd('theme_boost/drawer', 'init');
 
 // Layout.
--- lib.php
+++ lib.php
@@ -516,12 +516,14 @@ function theme_adaptable_performance_output($param) {
  */
 function theme_adaptable_page_init(moodle_page $page) {
     global $CFG;
-    $page->requires->jquery();
     // REMOVED: Deprecated function: error_log($CFG->version).
     /* if ($CFG->version < 2015051100) {
-        $page->requires->jquery_plugin('bootstrap', 'theme_adaptable');
         $page->requires->jquery_plugin('dropdown', 'theme_adaptable');
     } */
+
+//    $PAGE->requires->jquery_plugin('bootstrap', 'theme_boost');
+
+$page->requires->jquery();
 
     $page->requires->jquery_plugin('pace', 'theme_adaptable');
     $page->requires->jquery_plugin('flexslider', 'theme_adaptable');
