882ecd46f55520f539adad363d62ae2f72508b2f
3bits <info@3bits.es>
2019-07-09 12:23:06 +0200

    rebase

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->component = 'theme_adaptable';
 
 // Adaptable version date (YYYYMMDDrr where rr is the release number).
-$plugin->version   = 2019070100;
+$plugin->version   = 2019070101;
 
 // Moodle required version is 3.6.1.
 $plugin->requires  = 2018120301;
