a3e8b31f234129c32e66d922a0be3c7e91c71077
Manoj Solanki <ac5882@coventry.ac.uk>
2019-08-01 16:07:49 +0100

    Further fix #295.

--- config.php
+++ config.php
@@ -187,7 +187,7 @@ $THEME->layouts = array(
     // Part of course, typical for modules - default page layout if $cm specified in require_login().
     'incourse' => array(
         'file' => 'columns2.php',
-        'regions' => array('side-post', 'course-section-a   '),
+        'regions' => array('side-post', 'course-section-a'),
         'defaultregion' => 'side-post',
     ),
     // The site home page.
