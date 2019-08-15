7bed2e9a49a19316658a4ab729eea318874866ec
Jeremy Hopkins <jeremy.hopkins@outlook.com>
2019-08-15 17:41:30 +0000

    Update version.php to next fix release

--- version.php
+++ version.php
@@ -31,13 +31,13 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->component = 'theme_adaptable';
 
 // Adaptable version date (YYYYMMDDrr where rr is the release number).
-$plugin->version   = 2019080500;
+$plugin->version   = 2019081500;
 
 // Moodle required version is 3.6.1.
 $plugin->requires  = 2018120301;
 
 // Adaptable version using SemVer (https://semver.org).
-$plugin->release = '2.0.0';
+$plugin->release = '2.1.1';
 
 // Adaptable maturity (do not use ALPHA or BETA versions in production sites).
 $plugin->maturity = MATURITY_BETA;
