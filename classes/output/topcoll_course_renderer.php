b24f13a1fc62ed94454c8a72ae61f80a9c57cbf4
Gareth Barnard <gjbarnard@gmail.com>
2020-08-27 13:59:42 +0100

    Remove duplicate course_section_cm_name code - #158.

--- classes/output/topcoll_course_renderer.php
+++ classes/output/topcoll_course_renderer.php
@@ -61,6 +61,10 @@ class topcoll_course_renderer extends \theme_adaptable\output\core\course_render
             return '';
         }
 
+        if (!$mod->url) {
+            return '';
+        }
+
         // If use adaptable icons is set to false, then just run CT version of the method.
         if (empty($this->page->theme->settings->coursesectionactivityuseadaptableicons)) {
             list($linkclasses, $textclasses) = $this->course_section_cm_classes($mod);
@@ -73,92 +77,10 @@ class topcoll_course_renderer extends \theme_adaptable\output\core\course_render
                 $groupinglabel;
         }
 
-        $url = $mod->url;
-        if (!$url) {
-            return '';
-        }
-
-        // Accessibility: for files get description via icon, this is very ugly hack!
-        $instancename = $mod->get_formatted_name();
-        $altname = $mod->modfullname;
-        /* Avoid unnecessary duplication: if e.g. a forum name already
-           includes the word forum (or Forum, etc) then it is unhelpful
-           to include that in the accessible description that is added. */
-        if (false !== strpos(core_text::strtolower($instancename),
-                core_text::strtolower($altname))) {
-                    $altname = '';
-        }
-
-        // File type after name, for alphabetic lists (screen reader).
-        if ($altname) {
-            $altname = get_accesshide(' '.$altname);
-        }
-
-        /* For items which are hidden but available to current user
-          ($mod->uservisible), we show those as dimmed only if the user has
-           viewhiddenactivities, so that teachers see 'items which might not
-           be available to some students' dimmed but students do not see 'item
-           which is actually available to current student' dimmed. */
-        $linkclasses = '';
-        $accesstext = '';
-        $textclasses = '';
-        if ($mod->uservisible) {
-
-            $conditionalhidden = $this->is_cm_conditionally_hidden($mod);
-            $accessiblebutdim = (!$mod->visible || $conditionalhidden) &&
-            has_capability('moodle/course:viewhiddenactivities', $mod->context);
-            if ($accessiblebutdim) {
-                $linkclasses .= ' dimmed';
-                $textclasses .= ' dimmed_text';
-                if ($conditionalhidden) {
-                    $linkclasses .= ' conditionalhidden';
-                    $textclasses .= ' conditionalhidden';
-                }
-                // Show accessibility note only if user can access the module himself.
-                $accesstext = get_accesshide(get_string('hiddenfromstudents').':'. $mod->modfullname);
-            }
-
-        } else {
-
-            $linkclasses .= ' dimmed';
-            $textclasses .= ' dimmed_text';
-
-        }
-
-        // Get on-click attribute value if specified and decode the onclick - it
-        // has already been encoded for display (puke).
-        $onclick = htmlspecialchars_decode($mod->onclick, ENT_QUOTES);
-
-        $groupinglabel = $mod->get_grouping_label($textclasses);
-
-        // Display link itself.
-
-        // Get icon url, but strip -24, -64, -256  etc from the end of filetype icons so we
-        // only need to provide one SVG, see MDL-47082. (Used from snap theme).
-        $imageurl = \preg_replace('/-\d\d\d?$/', '', $mod->get_icon_url());
-
-        $activitylink = html_writer::empty_tag('img', array('src' => $imageurl,
-                'class' => 'iconlarge activityicon', 'alt' => ' ', 'role' => 'presentation')) . $accesstext .
-                html_writer::tag('span', $instancename . $altname, array('class' => 'instancename'));
-
-        $outputlink = '';
-        if ($mod->uservisible) {
-            $outputlink .= html_writer::link($url, $activitylink, array('class' => $linkclasses, 'onclick' => $onclick)) .
-            $groupinglabel;
-        } else {
-            // We may be displaying this just in order to show information
-            // about visibility, without the actual link ($mod->uservisible).
-            $outputlink .= html_writer::tag('div', $activitylink, array('class' => $textclasses)) .
-            $groupinglabel;
-        }
-
-        $tmpl = new \format_topcoll\output\course_module_name($mod, $this->page->user_is_editing(), $displayoptions);
-        $templatedata = $tmpl->export_for_template($this->output);
-
-        // Variable displayvalue element is purposely overriden below with link including custom icon created above.
-        $templatedata['displayvalue'] = $outputlink;
+        $templateclass = new \format_topcoll\output\course_module_name($mod, $this->page->user_is_editing(), $displayoptions);
+        $data = $this->adaptable_course_section_cm_name($mod, $templateclass);
 
         // Not sure about groupinglabel at end as same as CT but not Adaptable, need to see what happens.
-        return $this->output->render_from_template('core/inplace_editable', $templatedata).$groupinglabel;
+        return $this->output->render_from_template('core/inplace_editable', $data['templatedata']).$data['groupinglabel'];
     }
 }
