6644676e21c655da61d7a9bdd477eba5b3a13cd2
Gareth Barnard <gjbarnard@gmail.com>
2020-11-14 14:35:17 +0000

    Fixes.

--- db/upgrade.php
+++ db/upgrade.php
@@ -33,22 +33,19 @@ defined('MOODLE_INTERNAL') || die;
  * @return bool Success.
  */
 function xmldb_theme_adaptable_upgrade($oldversion = 0) {
-    global $CFG;
 
-    if ($CFG->branch == 39) {
-        if ($oldversion < 2020073101) {
-            if (get_config('theme_adaptable', 'fontname') == 'default') {
-                set_config('fontname', 'sans-serif', 'theme_adaptable');
-            }
-            if (get_config('theme_adaptable', 'fontheadername') == 'default') {
-                set_config('fontheadername', 'sans-serif', 'theme_adaptable');
-            }
-            if (get_config('theme_adaptable', 'fonttitlename') == 'default') {
-                set_config('fonttitlename', 'sans-serif', 'theme_adaptable');
-            }
+    if ($oldversion < 2020073101) {
+        if (get_config('theme_adaptable', 'fontname') == 'default') {
+            set_config('fontname', 'sans-serif', 'theme_adaptable');
         }
-    } else {
-        return false;
+        if (get_config('theme_adaptable', 'fontheadername') == 'default') {
+            set_config('fontheadername', 'sans-serif', 'theme_adaptable');
+        }
+        if (get_config('theme_adaptable', 'fonttitlename') == 'default') {
+            set_config('fonttitlename', 'sans-serif', 'theme_adaptable');
+        }
+
+        upgrade_plugin_savepoint(true, 2020073101, 'theme', 'adaptable');
     }
 
     // Automatic 'Purge all caches'....
