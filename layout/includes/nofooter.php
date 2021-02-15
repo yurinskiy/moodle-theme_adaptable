588f91932c2c683a0cb1098ae97bff825783fe06
Gareth Barnard <gjbarnard@gmail.com>
2021-02-15 20:50:46 +0000

    Travis.

--- layout/includes/nofooter.php
+++ layout/includes/nofooter.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 echo $OUTPUT->standard_after_main_region_html();
-echo '</div>'; // #page
-echo '</div>'; // #page-wrapper.
+echo '</div>'; // End #page
+echo '</div>'; // End #page-wrapper.
 echo $OUTPUT->standard_end_of_body_html();
 echo $OUTPUT->get_all_tracking_methods(); ?>
 <script type="text/javascript">
